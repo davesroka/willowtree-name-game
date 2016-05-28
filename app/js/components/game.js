0b1555dbad1c2e2d554ec41a6580cbccec331d8c
Dave Sroka <dave@davesroka.net>
2016-05-28 12:20:11 -0400

    Add teamMember component

--- app/js/components/game.js
+++ app/js/components/game.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import TeamMember from 'components/team-member/team-member';
 
 export default class Game extends React.Component {
 
   render() {
+    const teamMembers = [];
+
+    for (let i = 0; i < 11; i++) {
+      teamMembers.push(<TeamMember name={`Test Name ${i}`}/>);
+    }
+
     return (
       <div>
-        Hello this is dog
+        {teamMembers}
       </div>
     );
   }
---
+++ app/js/components/team-member/team-member.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class TeamMember extends React.Component {
+
+  render() {
+    return (
+      <div className="teamMember">
+        <h1>this.props.name</h1>
+      </div>
+    );
+  }
+}
