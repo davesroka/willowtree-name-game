a655bd08944d07cd4fd5e90f27a8def3cc8dc4f3
Dave Sroka <dsroka@wiredtriangle.com>
2016-06-01 22:32:07 -0400

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass'],
       },
+      {
+        test: /\.woff|woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
     ],
   },
   resolve: {
